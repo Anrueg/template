66d45d19af53ed7a835caa5d15c5f6f02097d599
Zoltan Vetesi <vetesi.zoltan@gmail.com>
2025-02-26 10:41:36 +0100

    feat: #angular:build

--- template/wizard/{% if frontend == 'angular' %}angular{% endif %}/script/sync-config.ts
+++ template/wizard/{% if frontend == 'angular' %}angular{% endif %}/script/sync-config.ts
@@ -51,25 +51,21 @@ function updateAngularConfig(packages: AngularPackage[]) {
     fs.writeFileSync("angular.json", JSON.stringify(result, null, 2))
 }
 
-function updateTsConfig(confPath: string, packages: AngularPackage[], pkgPath: string) {
+interface TsconfigPaths {
+    compilerOptions?: { paths?: Record<string, string[]> }
+}
+
+function tsconfigUpdatePaths(confPath: string, paths: Record<string, string>) {
     if (fs.existsSync(confPath)) {
-        const conf = JSON.parse(fs.readFileSync(confPath, "utf-8")) as {
-            compilerOptions?: { paths?: Record<string, string[]> }
-        }
-        if (!conf.compilerOptions) {
-            conf.compilerOptions = {}
-        }
+        const conf = JSON.parse(fs.readFileSync(confPath, "utf-8")) as TsconfigPaths
+        conf.compilerOptions ??= { paths: {} }
+        conf.compilerOptions.paths ??= {}
 
-        if (!conf.compilerOptions.paths) {
-            conf.compilerOptions.paths = {}
-        }
-
-        for (const pkg of packages) {
-            if (pkg.config["projectType"] === "application") {
-                continue
+        for (const [alias, pth] of Object.entries(paths)) {
+            const paths = (conf.compilerOptions.paths[alias] ??= [])
+            if (!paths.includes(pth)) {
+                paths.push(pth)
             }
-
-            conf.compilerOptions.paths[`@${AngularNs}/${pkg.name}`] = [`${pkgPath}${pkg.name}`]
         }
 
         fs.writeFileSync(confPath, JSON.stringify(conf, null, 2))
@@ -80,18 +76,27 @@ function main() {
     const packages = getPackages()
 
     updateAngularConfig(packages)
-    updateTsConfig("tsconfig.base.json", packages, "angular/")
+    tsconfigUpdatePaths(
+        "tsconfig.base.json",
+        packages.reduce<Record<string, string>>((dst, pkg) => {
+            dst[`@${AngularNs}/${pkg.name}`] = `angular/${pkg.name}/src/public-api.ts`
+            return dst
+        }, {})
+    )
 
     for (const pkg of packages) {
-        const tsconfig = path.join(pkg.path, "tsconfig.cli.json")
-        if (!fs.existsSync(tsconfig)) {
-            fs.writeFileSync(tsconfig, JSON.stringify({ extends: "./tsconfig.json" }, null, 2))
-        }
-        updateTsConfig(
-            tsconfig,
-            packages.filter(v => v.name !== pkg.name),
-            "dist/angular/"
+        // TODO: ng packager entrypoint
+        tsconfigUpdatePaths(
+            path.join(pkg.path, "tsconfig.cli.json"),
+            packages
+                .filter(v => v.config["projectType"] !== "application")
+                .filter(v => v.name !== pkg.name)
+                .reduce<Record<string, string>>((dst, pkg) => {
+                    dst[`@${AngularNs}/${pkg.name}`] = `dist/angular/${pkg.name}`
+                    return dst
+                }, {})
         )
+        // tsconfigUpdatePaths(path.join(pkg.path, "tsconfig.json"), {})
     }
 }
 
