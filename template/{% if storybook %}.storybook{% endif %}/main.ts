e768a498b227687196a690fc19b00c969e60cd59
Zoltan Vetesi <vetesi.zoltan@gmail.com>
2025-04-02 13:38:54 +0200

    fix(storybook): Allow import empty refs.json

--- template/{% if storybook %}.storybook{% endif %}/main.ts
+++ template/{% if storybook %}.storybook{% endif %}/main.ts
@@ -1,37 +1,23 @@
 import { type StorybookConfig } from "@storybook/react-vite"
-import { type StorybookConfigRaw } from "storybook/internal/types"
 import { mergeConfig, type ProxyOptions } from "vite"
 
-import refs from "./refs.json"
+import _refs from "./refs.json"
 
-type Refs = NonNullable<StorybookConfigRaw["refs"]>
+const refs = _refs as Refs
+type Refs = NonNullable<Record<string, { title: string; url: string; expanded?: boolean }>>
 type Proxy = Record<string, ProxyOptions>
-
-async function targetIsReachable(target: string): Promise<boolean> {
-    // wait for 2 minutes before giving up
-    for (let i = 0; i < 120; i++) {
-        try {
-            await fetch(target)
-        } catch {
-            await new Promise(r => setTimeout(r, 1000))
-            continue
-        }
-        return true
-    }
-    return false
-}
 
 export default {
     framework: "@storybook/react-vite",
     stories: ["./**/*.mdx", "./**/*.stories.@(ts|tsx)", "./**/*.story.@(ts|tsx)"],
     core: { builder: "@storybook/builder-vite" },
     addons: ["@storybook/addon-essentials"],
-    refs: (Object.keys(refs) as Array<keyof typeof refs>).reduce<Refs>((dst, key) => {
+    refs: Object.keys(refs).reduce<Refs>((dst, key) => {
         dst[key] = { title: refs[key].title, url: `-/${key}`, expanded: true }
         return dst
     }, {}),
     viteFinal(config) {
-        const proxy = (Object.keys(refs) as Array<keyof typeof refs>).reduce<Proxy>((dst, key) => {
+        const proxy = Object.keys(refs).reduce<Proxy>((dst, key) => {
             const prefix = `/-/${key}`
             const target = refs[key].url
             dst[prefix] = {
@@ -51,3 +37,17 @@ export default {
         })
     }
 } satisfies StorybookConfig
+
+async function targetIsReachable(target: string): Promise<boolean> {
+    // wait for 2 minutes before giving up
+    for (let i = 0; i < 120; i++) {
+        try {
+            await fetch(target)
+        } catch {
+            await new Promise(r => setTimeout(r, 1000))
+            continue
+        }
+        return true
+    }
+    return false
+}
